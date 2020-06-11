4ebdf005f3b37ab892eab62463faff04ef7acedb
SemyonWinter <60749163+SemyonWinter@users.noreply.github.com>
2020-06-11 19:00:55 +0700

    fix fix

--- number_fractal_in.cpp
+++ number_fractal_in.cpp
@@ -8,7 +8,7 @@ number_fractal *inFract(std::ifstream & ifstr)
 	ifstr >> num->numerator_int;
 	if (ifstr.eof() || ifstr.fail() || ifstr.bad()) found_err = true;
 	ifstr >> num->denominator_int;
-	if (ifstr.eof() || ifstr.fail() || ifstr.bad()) found_err = true;
+	if (ifstr.eof() || ifstr.fail() || ifstr.bad() || num->denominator_int == 0) found_err = true;
 	ifstr >> num->measure;
 	if (ifstr.fail() || ifstr.bad() || found_err) {
 		delete num;
